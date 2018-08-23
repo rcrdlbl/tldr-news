5b1bf4ea2461587eab098bc551de858bdf41a5a1
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-23 13:04:46 +0000

    Interface outline

--- lib/tldr_news/cli.rb
+++ lib/tldr_news/cli.rb
@@ -2,4 +2,16 @@ class TldrNews::CLI
   def call
     puts "TLDR NEWS:"
   end
+
+  def headline_list
+    #this will put out a list of headlines
+  end
+
+  def article_info
+    #this will show the article with a link to the full article
+  end
+
+  def menu
+    #this will let the user select an article to read more about
+  end
 end
